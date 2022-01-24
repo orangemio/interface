2564901741583fd03dc2e2427c038cd8c21b9809
undefind <undefind@163.com>
2022-01-24 11:26:36 +0800

    mb

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -19,13 +19,21 @@ export const StyledEthereumLogo = styled.img<{ size: string }>`
   height: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
   border-radius: 24px;
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    width: ${({ size }) => parseInt(size) / 2 + "px"};
+    height: ${({ size }) => parseInt(size) / 2 + "px"};
+`};
 `
 
-const StyledLogo = styled(Logo)<{ size: string }>`
+const StyledLogo = styled(Logo) <{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   border-radius: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    width: ${({ size }) => parseInt(size) / 2 + "px"};
+    height: ${({ size }) => parseInt(size) / 2 + "px"};
+  `};
 `
 
 export default function CurrencyLogo({
