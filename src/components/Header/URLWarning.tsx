c8ada9d6025c129b28b60f79f3cd79b791c6f0f0
vdovaT <vdova@protonmail.com>
2022-03-16 03:10:53 +0800

    update

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -32,7 +32,7 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> {t('header.makeSureURLWarning')}
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>pizaswap.com & www.pizaswap.com</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}> eth.pizaswap.com </code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
